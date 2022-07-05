65069269b2a66784524ad07fdf589d661f2285ec
GustavoCarvalhopro <gustavor.carvalho@hotmail.com>
2022-07-05 04:46:44 -0300

    second commit

--- televisao.py
+++ televisao.py
@@ -1,6 +1,7 @@
 class Televisao:
     def __init__(self):
         self.ligada = False
+        self.canal = 5
 
     def power(self):
         if self.ligada:
@@ -8,12 +9,25 @@ class Televisao:
         else:
             self.ligada = True    
 
+    def aumenta_canal(self):
+        if self.ligada == True:
+            self.canal +=1    
+
+    def diminui_canal(self):
+        if self.ligada ==True:
+            self.canal -=1            
+
 
 televisao = Televisao()
 print('A Televisão está ligada? {}'.format(televisao.ligada))   
 televisao.power()
 print('A Televisão está ligada? {}'.format(televisao.ligada))
 televisao.power()
-print('A Televisão está ligada? {}'.format(televisao.ligada))         
-
+print('A Televisão está ligada? {}'.format(televisao.ligada))     
+televisao.aumenta_canal() 
+print(televisao.canal)
+televisao.power()   
+print('A Televisão está ligada: {}'.format(televisao.ligada))
+televisao.aumenta_canal()
+print(televisao.canal)
    
